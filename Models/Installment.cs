eff55887085b2cd9d8f124f676bf48b953d45e4c
Nkosana <nkosana.mabuza2@gmail.com>
2025-10-29 16:33:12 +0200

    designing the loan view

--- Models/Installment.cs
+++ Models/Installment.cs
@@ -1,7 +1,7 @@
 public class Installment{
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string ApplicantId{ get; set; } = "";
-
+    public int Amount{get; set;}
     public DateTime PaymentDate { get; set; } = DateTime.UtcNow;
     public string Status { get; set; } = "";
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
