e39695413a5cc93d32f5be2e7c50dd83e19cc5aa
Nkosana <nkosana.mabuza2@gmail.com>
2026-07-02 16:50:52 +0200

    Updating the loan card component

---
+++ Models/LoanInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class LoanInfo
+    {
+        public string Title { get; set; } ="";
+        public string Description { get; set; } ="";
+        public string Icon { get; set; } ="";
+        public string SmallDescription { get; set; } ="";
+        public LoanTypes LoanType{get;set;} = LoanTypes.None;
+    }
+}
